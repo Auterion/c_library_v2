31668164f0bf9fe3c5605e7b3cac962c1b9c9aa7
Michael Schaeuble <schaeuble.michael@gmail.com>
2018-09-17 16:12:39 +0200

    Include UTM_DATA_AVAIL_FLAGS_RELATIVE_ALTITUDE_AVAILABLE flag

--- common/common.h
+++ common/common.h
@@ -1267,11 +1267,12 @@ typedef enum UTM_FLIGHT_STATE
 typedef enum UTM_DATA_AVAIL_FLAGS
 {
    UTM_DATA_AVAIL_FLAGS_TIME_VALID=1, /* The field time contains valid data. | */
-   UTM_DATA_AVAIL_FLAGS_UAS_ID_AVAILABLE=2, /* The field uasId contains valid data. | */
-   UTM_DATA_AVAIL_FLAGS_POSITION_AVAILABLE=4, /* The fields lat, lon, alt and relativeAlt contain valid data. | */
-   UTM_DATA_AVAIL_FLAGS_ALTITUDE_AVAILABLE=8, /* The fields lat, lon, alt and relativeAlt contain valid data. | */
-   UTM_DATA_AVAIL_FLAGS_NEXT_WAYPOINT_AVAILABLE=16, /* The fields next_at, next_lon and next_alt contain valid data. | */
-   UTM_DATA_AVAIL_FLAGS_ENUM_END=17, /*  | */
+   UTM_DATA_AVAIL_FLAGS_UAS_ID_AVAILABLE=2, /* The field uas_id contains valid data. | */
+   UTM_DATA_AVAIL_FLAGS_POSITION_AVAILABLE=4, /* The fields lat, lon and h_acc contain valid data. | */
+   UTM_DATA_AVAIL_FLAGS_ALTITUDE_AVAILABLE=8, /* The fields alt and v_acc contain valid data. | */
+   UTM_DATA_AVAIL_FLAGS_RELATIVE_ALTITUDE_AVAILABLE=16, /* The field relative_alt contains valid data. | */
+   UTM_DATA_AVAIL_FLAGS_NEXT_WAYPOINT_AVAILABLE=32, /* The fields next_lat, next_lon and next_alt contain valid data. | */
+   UTM_DATA_AVAIL_FLAGS_ENUM_END=33, /*  | */
 } UTM_DATA_AVAIL_FLAGS;
 #endif
 
