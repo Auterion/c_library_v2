cfad977ae4a99f5e7cdf74acf12cfc541ccec10c
Mohamad Akkawi <akkawi@protonmail.com>
2023-01-30 20:39:56 -0500

    Extend cellular_status msg and add new nic_info msg for static fields a361f588f04cc1add366620970dd0f9fb0af838a

--- minimal/version.h
+++ minimal/version.h
@@ -3,12 +3,12 @@
  *  @see http://mavlink.org
  */
 #pragma once
- 
+
 #ifndef MAVLINK_VERSION_H
 #define MAVLINK_VERSION_H
 
-#define MAVLINK_BUILD_DATE "Mon Jul 18 2022"
+#define MAVLINK_BUILD_DATE "Mon Jan 30 2023"
 #define MAVLINK_WIRE_PROTOCOL_VERSION "2.0"
 #define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 22
- 
+
 #endif // MAVLINK_VERSION_H
