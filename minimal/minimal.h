0c30bdf25cc8087f81b012fb3a2892639fad116e
auterionci <ci@auterion.com>
2025-05-27 06:39:40 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/79964a6aaba58461a9d1e2106c465f41ca47493b

--- minimal/minimal.h
+++ minimal/minimal.h
@@ -114,7 +114,8 @@ typedef enum MAV_TYPE
    MAV_TYPE_GENERIC_MULTIROTOR=43, /* Generic multirotor that does not fit into a specific type or whose type is unknown | */
    MAV_TYPE_ILLUMINATOR=44, /* Illuminator. An illuminator is a light source that is used for lighting up dark areas external to the sytstem: e.g. a torch or searchlight (as opposed to a light source for illuminating the system itself, e.g. an indicator light). | */
    MAV_TYPE_SPACECRAFT_ORBITER=45, /* Orbiter spacecraft. Includes satellites orbiting terrestrial and extra-terrestrial bodies. Follows NASA Spacecraft Classification. | */
-   MAV_TYPE_ENUM_END=46, /*  | */
+   MAV_TYPE_GROUND_QUADRUPED=46, /* A generic four-legged ground vehicle (e.g., a robot dog). | */
+   MAV_TYPE_ENUM_END=47, /*  | */
 } MAV_TYPE;
 #endif
 
