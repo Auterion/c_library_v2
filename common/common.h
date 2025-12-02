8b0f65e8de92d7ffb30645405832803522f20465
auterionci <ci@auterion.com>
2025-12-02 08:29:16 +0000

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/9e0fc9e6e3e66365dfd89a411b36cfb4b0ff7c12

--- common/common.h
+++ common/common.h
@@ -10,7 +10,7 @@
     #error Wrong include order: MAVLINK_COMMON.H MUST NOT BE DIRECTLY USED. Include mavlink.h from the same directory instead or set ALL AND EVERY defines from MAVLINK.H manually accordingly, including the #define MAVLINK_H call.
 #endif
 
-#define MAVLINK_COMMON_XML_HASH 2746272052607405440
+#define MAVLINK_COMMON_XML_HASH -459641445833735202
 
 #ifdef __cplusplus
 extern "C" {
