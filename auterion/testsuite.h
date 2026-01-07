34d4fdde743ecc48801ac852ea8a44bdcb00286a
Stuart Miller <stuart@auterion.com>
2026-01-07 13:29:05 -0600

    autogenerated headers for rev https://github.com/auterion/mavlink/tree/06db08699d04b9f011afa1fa3712536ac772ce0e

--- auterion/testsuite.h
+++ auterion/testsuite.h
@@ -1727,6 +1727,68 @@ static void mavlink_test_radar_target_track(uint8_t system_id, uint8_t component
 #endif
 }
 
+static void mavlink_test_payload_device_status(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+    mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_PAYLOAD_DEVICE_STATUS >= 256) {
+            return;
+        }
+#endif
+    mavlink_message_t msg;
+        uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
+        uint16_t i;
+    mavlink_payload_device_status_t packet_in = {
+        963497464,17,84,"GHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQ"
+    };
+    mavlink_payload_device_status_t packet1, packet2;
+        memset(&packet1, 0, sizeof(packet1));
+        packet1.countdown = packet_in.countdown;
+        packet1.state = packet_in.state;
+        packet1.mode = packet_in.mode;
+        
+        mav_array_memcpy(packet1.message, packet_in.message, sizeof(char)*64);
+        
+#ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
+        if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
+           // cope with extensions
+           memset(MAVLINK_MSG_ID_PAYLOAD_DEVICE_STATUS_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_PAYLOAD_DEVICE_STATUS_MIN_LEN);
+        }
+#endif
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_payload_device_status_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_payload_device_status_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_payload_device_status_pack(system_id, component_id, &msg , packet1.state , packet1.countdown , packet1.mode , packet1.message );
+    mavlink_msg_payload_device_status_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_payload_device_status_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.state , packet1.countdown , packet1.mode , packet1.message );
+    mavlink_msg_payload_device_status_decode(&msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+        memset(&packet2, 0, sizeof(packet2));
+        mavlink_msg_to_send_buffer(buffer, &msg);
+        for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
+            comm_send_ch(MAVLINK_COMM_0, buffer[i]);
+        }
+    mavlink_msg_payload_device_status_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+        
+        memset(&packet2, 0, sizeof(packet2));
+    mavlink_msg_payload_device_status_send(MAVLINK_COMM_1 , packet1.state , packet1.countdown , packet1.mode , packet1.message );
+    mavlink_msg_payload_device_status_decode(last_msg, &packet2);
+        MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
+
+#ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
+    MAVLINK_ASSERT(mavlink_get_message_info_by_name("PAYLOAD_DEVICE_STATUS") != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_PAYLOAD_DEVICE_STATUS) != NULL);
+#endif
+}
+
 static void mavlink_test_auterion(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
     mavlink_test_param_ack_transaction(system_id, component_id, last_msg);
@@ -1756,6 +1818,7 @@ static void mavlink_test_auterion(uint8_t system_id, uint8_t component_id, mavli
     mavlink_test_water_depth_raw(system_id, component_id, last_msg);
     mavlink_test_wind_data_raw(system_id, component_id, last_msg);
     mavlink_test_radar_target_track(system_id, component_id, last_msg);
+    mavlink_test_payload_device_status(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
