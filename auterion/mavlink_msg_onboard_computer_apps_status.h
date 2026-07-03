efe9fc764cdcd4e4fc9ab2a1a1f1d0a370a85d44
karol <karol.fuks@auterion.com>
2026-07-03 14:32:11 +0200

    Regenerate auterion dialect: rename 13802, 13801 total_apps_count
    13802 ONBOARD_COMPUTER_APP_FAILURE -> ONBOARD_COMPUTER_APP_HEALTH, now one message per bundled app (healthy = failure_flags 0); 13801 drops failing_apps_count for total_apps_count.

--- auterion/mavlink_msg_onboard_computer_apps_status.h
+++ auterion/mavlink_msg_onboard_computer_apps_status.h
@@ -11,8 +11,9 @@ typedef struct __mavlink_onboard_computer_apps_status_t {
  uint8_t failure_flags; /*<  
         Bitmask of detected failure types across all default bundled apps
       */
- uint8_t failing_apps_count; /*<  
-        Number of default bundled apps in failure state
+ uint8_t total_apps_count; /*<  
+        Total number of default bundled apps tracked (number of
+        ONBOARD_COMPUTER_APP_HEALTH messages to expect on request)
       */
 } mavlink_onboard_computer_apps_status_t;
 
@@ -21,8 +22,8 @@ typedef struct __mavlink_onboard_computer_apps_status_t {
 #define MAVLINK_MSG_ID_13801_LEN 3
 #define MAVLINK_MSG_ID_13801_MIN_LEN 3
 
-#define MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_CRC 251
-#define MAVLINK_MSG_ID_13801_CRC 251
+#define MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_CRC 26
+#define MAVLINK_MSG_ID_13801_CRC 26
 
 
 
@@ -33,7 +34,7 @@ typedef struct __mavlink_onboard_computer_apps_status_t {
     3, \
     {  { "overall_health", NULL, MAVLINK_TYPE_UINT8_T, 0, 0, offsetof(mavlink_onboard_computer_apps_status_t, overall_health) }, \
          { "failure_flags", NULL, MAVLINK_TYPE_UINT8_T, 0, 1, offsetof(mavlink_onboard_computer_apps_status_t, failure_flags) }, \
-         { "failing_apps_count", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_onboard_computer_apps_status_t, failing_apps_count) }, \
+         { "total_apps_count", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_onboard_computer_apps_status_t, total_apps_count) }, \
          } \
 }
 #else
@@ -42,7 +43,7 @@ typedef struct __mavlink_onboard_computer_apps_status_t {
     3, \
     {  { "overall_health", NULL, MAVLINK_TYPE_UINT8_T, 0, 0, offsetof(mavlink_onboard_computer_apps_status_t, overall_health) }, \
          { "failure_flags", NULL, MAVLINK_TYPE_UINT8_T, 0, 1, offsetof(mavlink_onboard_computer_apps_status_t, failure_flags) }, \
-         { "failing_apps_count", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_onboard_computer_apps_status_t, failing_apps_count) }, \
+         { "total_apps_count", NULL, MAVLINK_TYPE_UINT8_T, 0, 2, offsetof(mavlink_onboard_computer_apps_status_t, total_apps_count) }, \
          } \
 }
 #endif
@@ -59,27 +60,28 @@ typedef struct __mavlink_onboard_computer_apps_status_t {
  * @param failure_flags  
         Bitmask of detected failure types across all default bundled apps
       
- * @param failing_apps_count  
-        Number of default bundled apps in failure state
+ * @param total_apps_count  
+        Total number of default bundled apps tracked (number of
+        ONBOARD_COMPUTER_APP_HEALTH messages to expect on request)
       
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 MAVLINK_WIP
 static inline uint16_t mavlink_msg_onboard_computer_apps_status_pack(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg,
-                               uint8_t overall_health, uint8_t failure_flags, uint8_t failing_apps_count)
+                               uint8_t overall_health, uint8_t failure_flags, uint8_t total_apps_count)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN];
     _mav_put_uint8_t(buf, 0, overall_health);
     _mav_put_uint8_t(buf, 1, failure_flags);
-    _mav_put_uint8_t(buf, 2, failing_apps_count);
+    _mav_put_uint8_t(buf, 2, total_apps_count);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN);
 #else
     mavlink_onboard_computer_apps_status_t packet;
     packet.overall_health = overall_health;
     packet.failure_flags = failure_flags;
-    packet.failing_apps_count = failing_apps_count;
+    packet.total_apps_count = total_apps_count;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN);
 #endif
@@ -101,26 +103,27 @@ static inline uint16_t mavlink_msg_onboard_computer_apps_status_pack(uint8_t sys
  * @param failure_flags  
         Bitmask of detected failure types across all default bundled apps
       
- * @param failing_apps_count  
-        Number of default bundled apps in failure state
+ * @param total_apps_count  
+        Total number of default bundled apps tracked (number of
+        ONBOARD_COMPUTER_APP_HEALTH messages to expect on request)
       
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 static inline uint16_t mavlink_msg_onboard_computer_apps_status_pack_status(uint8_t system_id, uint8_t component_id, mavlink_status_t *_status, mavlink_message_t* msg,
-                               uint8_t overall_health, uint8_t failure_flags, uint8_t failing_apps_count)
+                               uint8_t overall_health, uint8_t failure_flags, uint8_t total_apps_count)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN];
     _mav_put_uint8_t(buf, 0, overall_health);
     _mav_put_uint8_t(buf, 1, failure_flags);
-    _mav_put_uint8_t(buf, 2, failing_apps_count);
+    _mav_put_uint8_t(buf, 2, total_apps_count);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN);
 #else
     mavlink_onboard_computer_apps_status_t packet;
     packet.overall_health = overall_health;
     packet.failure_flags = failure_flags;
-    packet.failing_apps_count = failing_apps_count;
+    packet.total_apps_count = total_apps_count;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN);
 #endif
@@ -145,28 +148,29 @@ static inline uint16_t mavlink_msg_onboard_computer_apps_status_pack_status(uint
  * @param failure_flags  
         Bitmask of detected failure types across all default bundled apps
       
- * @param failing_apps_count  
-        Number of default bundled apps in failure state
+ * @param total_apps_count  
+        Total number of default bundled apps tracked (number of
+        ONBOARD_COMPUTER_APP_HEALTH messages to expect on request)
       
  * @return length of the message in bytes (excluding serial stream start sign)
  */
 MAVLINK_WIP
 static inline uint16_t mavlink_msg_onboard_computer_apps_status_pack_chan(uint8_t system_id, uint8_t component_id, uint8_t chan,
                                mavlink_message_t* msg,
-                                   uint8_t overall_health,uint8_t failure_flags,uint8_t failing_apps_count)
+                                   uint8_t overall_health,uint8_t failure_flags,uint8_t total_apps_count)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN];
     _mav_put_uint8_t(buf, 0, overall_health);
     _mav_put_uint8_t(buf, 1, failure_flags);
-    _mav_put_uint8_t(buf, 2, failing_apps_count);
+    _mav_put_uint8_t(buf, 2, total_apps_count);
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), buf, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN);
 #else
     mavlink_onboard_computer_apps_status_t packet;
     packet.overall_health = overall_health;
     packet.failure_flags = failure_flags;
-    packet.failing_apps_count = failing_apps_count;
+    packet.total_apps_count = total_apps_count;
 
         memcpy(_MAV_PAYLOAD_NON_CONST(msg), &packet, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN);
 #endif
@@ -186,7 +190,7 @@ static inline uint16_t mavlink_msg_onboard_computer_apps_status_pack_chan(uint8_
 MAVLINK_WIP
 static inline uint16_t mavlink_msg_onboard_computer_apps_status_encode(uint8_t system_id, uint8_t component_id, mavlink_message_t* msg, const mavlink_onboard_computer_apps_status_t* onboard_computer_apps_status)
 {
-    return mavlink_msg_onboard_computer_apps_status_pack(system_id, component_id, msg, onboard_computer_apps_status->overall_health, onboard_computer_apps_status->failure_flags, onboard_computer_apps_status->failing_apps_count);
+    return mavlink_msg_onboard_computer_apps_status_pack(system_id, component_id, msg, onboard_computer_apps_status->overall_health, onboard_computer_apps_status->failure_flags, onboard_computer_apps_status->total_apps_count);
 }
 
 /**
@@ -201,7 +205,7 @@ static inline uint16_t mavlink_msg_onboard_computer_apps_status_encode(uint8_t s
 MAVLINK_WIP
 static inline uint16_t mavlink_msg_onboard_computer_apps_status_encode_chan(uint8_t system_id, uint8_t component_id, uint8_t chan, mavlink_message_t* msg, const mavlink_onboard_computer_apps_status_t* onboard_computer_apps_status)
 {
-    return mavlink_msg_onboard_computer_apps_status_pack_chan(system_id, component_id, chan, msg, onboard_computer_apps_status->overall_health, onboard_computer_apps_status->failure_flags, onboard_computer_apps_status->failing_apps_count);
+    return mavlink_msg_onboard_computer_apps_status_pack_chan(system_id, component_id, chan, msg, onboard_computer_apps_status->overall_health, onboard_computer_apps_status->failure_flags, onboard_computer_apps_status->total_apps_count);
 }
 
 /**
@@ -215,7 +219,7 @@ static inline uint16_t mavlink_msg_onboard_computer_apps_status_encode_chan(uint
  */
 static inline uint16_t mavlink_msg_onboard_computer_apps_status_encode_status(uint8_t system_id, uint8_t component_id, mavlink_status_t* _status, mavlink_message_t* msg, const mavlink_onboard_computer_apps_status_t* onboard_computer_apps_status)
 {
-    return mavlink_msg_onboard_computer_apps_status_pack_status(system_id, component_id, _status, msg,  onboard_computer_apps_status->overall_health, onboard_computer_apps_status->failure_flags, onboard_computer_apps_status->failing_apps_count);
+    return mavlink_msg_onboard_computer_apps_status_pack_status(system_id, component_id, _status, msg,  onboard_computer_apps_status->overall_health, onboard_computer_apps_status->failure_flags, onboard_computer_apps_status->total_apps_count);
 }
 
 /**
@@ -228,27 +232,28 @@ static inline uint16_t mavlink_msg_onboard_computer_apps_status_encode_status(ui
  * @param failure_flags  
         Bitmask of detected failure types across all default bundled apps
       
- * @param failing_apps_count  
-        Number of default bundled apps in failure state
+ * @param total_apps_count  
+        Total number of default bundled apps tracked (number of
+        ONBOARD_COMPUTER_APP_HEALTH messages to expect on request)
       
  */
 #ifdef MAVLINK_USE_CONVENIENCE_FUNCTIONS
 
 MAVLINK_WIP
-static inline void mavlink_msg_onboard_computer_apps_status_send(mavlink_channel_t chan, uint8_t overall_health, uint8_t failure_flags, uint8_t failing_apps_count)
+static inline void mavlink_msg_onboard_computer_apps_status_send(mavlink_channel_t chan, uint8_t overall_health, uint8_t failure_flags, uint8_t total_apps_count)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char buf[MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN];
     _mav_put_uint8_t(buf, 0, overall_health);
     _mav_put_uint8_t(buf, 1, failure_flags);
-    _mav_put_uint8_t(buf, 2, failing_apps_count);
+    _mav_put_uint8_t(buf, 2, total_apps_count);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS, buf, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_MIN_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_CRC);
 #else
     mavlink_onboard_computer_apps_status_t packet;
     packet.overall_health = overall_health;
     packet.failure_flags = failure_flags;
-    packet.failing_apps_count = failing_apps_count;
+    packet.total_apps_count = total_apps_count;
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS, (const char *)&packet, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_MIN_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_CRC);
 #endif
@@ -263,7 +268,7 @@ MAVLINK_WIP
 static inline void mavlink_msg_onboard_computer_apps_status_send_struct(mavlink_channel_t chan, const mavlink_onboard_computer_apps_status_t* onboard_computer_apps_status)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
-    mavlink_msg_onboard_computer_apps_status_send(chan, onboard_computer_apps_status->overall_health, onboard_computer_apps_status->failure_flags, onboard_computer_apps_status->failing_apps_count);
+    mavlink_msg_onboard_computer_apps_status_send(chan, onboard_computer_apps_status->overall_health, onboard_computer_apps_status->failure_flags, onboard_computer_apps_status->total_apps_count);
 #else
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS, (const char *)onboard_computer_apps_status, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_MIN_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_CRC);
 #endif
@@ -278,20 +283,20 @@ static inline void mavlink_msg_onboard_computer_apps_status_send_struct(mavlink_
   incoming message with minimum stack space usage.
  */
 MAVLINK_WIP
-static inline void mavlink_msg_onboard_computer_apps_status_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint8_t overall_health, uint8_t failure_flags, uint8_t failing_apps_count)
+static inline void mavlink_msg_onboard_computer_apps_status_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint8_t overall_health, uint8_t failure_flags, uint8_t total_apps_count)
 {
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     char *buf = (char *)msgbuf;
     _mav_put_uint8_t(buf, 0, overall_health);
     _mav_put_uint8_t(buf, 1, failure_flags);
-    _mav_put_uint8_t(buf, 2, failing_apps_count);
+    _mav_put_uint8_t(buf, 2, total_apps_count);
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS, buf, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_MIN_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_CRC);
 #else
     mavlink_onboard_computer_apps_status_t *packet = (mavlink_onboard_computer_apps_status_t *)msgbuf;
     packet->overall_health = overall_health;
     packet->failure_flags = failure_flags;
-    packet->failing_apps_count = failing_apps_count;
+    packet->total_apps_count = total_apps_count;
 
     _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS, (const char *)packet, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_MIN_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_CRC);
 #endif
@@ -330,14 +335,15 @@ static inline uint8_t mavlink_msg_onboard_computer_apps_status_get_failure_flags
 }
 
 /**
- * @brief Get field failing_apps_count from onboard_computer_apps_status message
+ * @brief Get field total_apps_count from onboard_computer_apps_status message
  *
  * @return  
-        Number of default bundled apps in failure state
+        Total number of default bundled apps tracked (number of
+        ONBOARD_COMPUTER_APP_HEALTH messages to expect on request)
       
  */
 MAVLINK_WIP
-static inline uint8_t mavlink_msg_onboard_computer_apps_status_get_failing_apps_count(const mavlink_message_t* msg)
+static inline uint8_t mavlink_msg_onboard_computer_apps_status_get_total_apps_count(const mavlink_message_t* msg)
 {
     return _MAV_RETURN_uint8_t(msg,  2);
 }
@@ -354,7 +360,7 @@ static inline void mavlink_msg_onboard_computer_apps_status_decode(const mavlink
 #if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
     onboard_computer_apps_status->overall_health = mavlink_msg_onboard_computer_apps_status_get_overall_health(msg);
     onboard_computer_apps_status->failure_flags = mavlink_msg_onboard_computer_apps_status_get_failure_flags(msg);
-    onboard_computer_apps_status->failing_apps_count = mavlink_msg_onboard_computer_apps_status_get_failing_apps_count(msg);
+    onboard_computer_apps_status->total_apps_count = mavlink_msg_onboard_computer_apps_status_get_total_apps_count(msg);
 #else
         uint8_t len = msg->len < MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN? msg->len : MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN;
         memset(onboard_computer_apps_status, 0, MAVLINK_MSG_ID_ONBOARD_COMPUTER_APPS_STATUS_LEN);
--- auterion/testsuite.h
+++ auterion/testsuite.h
@@ -1804,7 +1804,7 @@ static void mavlink_test_onboard_computer_apps_status(uint8_t system_id, uint8_t
         memset(&packet1, 0, sizeof(packet1));
         packet1.overall_health = packet_in.overall_health;
         packet1.failure_flags = packet_in.failure_flags;
-        packet1.failing_apps_count = packet_in.failing_apps_count;
+        packet1.total_apps_count = packet_in.total_apps_count;
         
         
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
@@ -1819,12 +1819,12 @@ static void mavlink_test_onboard_computer_apps_status(uint8_t system_id, uint8_t
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_onboard_computer_apps_status_pack(system_id, component_id, &msg , packet1.overall_health , packet1.failure_flags , packet1.failing_apps_count );
+    mavlink_msg_onboard_computer_apps_status_pack(system_id, component_id, &msg , packet1.overall_health , packet1.failure_flags , packet1.total_apps_count );
     mavlink_msg_onboard_computer_apps_status_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_onboard_computer_apps_status_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.overall_health , packet1.failure_flags , packet1.failing_apps_count );
+    mavlink_msg_onboard_computer_apps_status_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.overall_health , packet1.failure_flags , packet1.total_apps_count );
     mavlink_msg_onboard_computer_apps_status_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
@@ -1837,7 +1837,7 @@ static void mavlink_test_onboard_computer_apps_status(uint8_t system_id, uint8_t
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
         
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_onboard_computer_apps_status_send(MAVLINK_COMM_1 , packet1.overall_health , packet1.failure_flags , packet1.failing_apps_count );
+    mavlink_msg_onboard_computer_apps_status_send(MAVLINK_COMM_1 , packet1.overall_health , packet1.failure_flags , packet1.total_apps_count );
     mavlink_msg_onboard_computer_apps_status_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
@@ -1847,21 +1847,21 @@ static void mavlink_test_onboard_computer_apps_status(uint8_t system_id, uint8_t
 #endif
 }
 
-static void mavlink_test_onboard_computer_app_failure(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+static void mavlink_test_onboard_computer_app_health(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
     mavlink_status_t *status = mavlink_get_channel_status(MAVLINK_COMM_0);
-        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_ONBOARD_COMPUTER_APP_FAILURE >= 256) {
+        if ((status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) && MAVLINK_MSG_ID_ONBOARD_COMPUTER_APP_HEALTH >= 256) {
             return;
         }
 #endif
     mavlink_message_t msg;
         uint8_t buffer[MAVLINK_MAX_PACKET_LEN];
         uint16_t i;
-    mavlink_onboard_computer_app_failure_t packet_in = {
+    mavlink_onboard_computer_app_health_t packet_in = {
         "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVW",133,"ZABCDEFGHIJKLMNOPQRSTUVWXYZABCD","FGHIJKLMNOPQRSTUVWXYZABCDEFGHIJ"
     };
-    mavlink_onboard_computer_app_failure_t packet1, packet2;
+    mavlink_onboard_computer_app_health_t packet1, packet2;
         memset(&packet1, 0, sizeof(packet1));
         packet1.failure_flags = packet_in.failure_flags;
         
@@ -1872,22 +1872,22 @@ static void mavlink_test_onboard_computer_app_failure(uint8_t system_id, uint8_t
 #ifdef MAVLINK_STATUS_FLAG_OUT_MAVLINK1
         if (status->flags & MAVLINK_STATUS_FLAG_OUT_MAVLINK1) {
            // cope with extensions
-           memset(MAVLINK_MSG_ID_ONBOARD_COMPUTER_APP_FAILURE_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_ONBOARD_COMPUTER_APP_FAILURE_MIN_LEN);
+           memset(MAVLINK_MSG_ID_ONBOARD_COMPUTER_APP_HEALTH_MIN_LEN + (char *)&packet1, 0, sizeof(packet1)-MAVLINK_MSG_ID_ONBOARD_COMPUTER_APP_HEALTH_MIN_LEN);
         }
 #endif
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_onboard_computer_app_failure_encode(system_id, component_id, &msg, &packet1);
-    mavlink_msg_onboard_computer_app_failure_decode(&msg, &packet2);
+    mavlink_msg_onboard_computer_app_health_encode(system_id, component_id, &msg, &packet1);
+    mavlink_msg_onboard_computer_app_health_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_onboard_computer_app_failure_pack(system_id, component_id, &msg , packet1.app_name , packet1.failure_flags , packet1.expected_version , packet1.actual_version );
-    mavlink_msg_onboard_computer_app_failure_decode(&msg, &packet2);
+    mavlink_msg_onboard_computer_app_health_pack(system_id, component_id, &msg , packet1.app_name , packet1.failure_flags , packet1.expected_version , packet1.actual_version );
+    mavlink_msg_onboard_computer_app_health_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_onboard_computer_app_failure_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.app_name , packet1.failure_flags , packet1.expected_version , packet1.actual_version );
-    mavlink_msg_onboard_computer_app_failure_decode(&msg, &packet2);
+    mavlink_msg_onboard_computer_app_health_pack_chan(system_id, component_id, MAVLINK_COMM_0, &msg , packet1.app_name , packet1.failure_flags , packet1.expected_version , packet1.actual_version );
+    mavlink_msg_onboard_computer_app_health_decode(&msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
         memset(&packet2, 0, sizeof(packet2));
@@ -1895,17 +1895,17 @@ static void mavlink_test_onboard_computer_app_failure(uint8_t system_id, uint8_t
         for (i=0; i<mavlink_msg_get_send_buffer_length(&msg); i++) {
             comm_send_ch(MAVLINK_COMM_0, buffer[i]);
         }
-    mavlink_msg_onboard_computer_app_failure_decode(last_msg, &packet2);
+    mavlink_msg_onboard_computer_app_health_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
         
         memset(&packet2, 0, sizeof(packet2));
-    mavlink_msg_onboard_computer_app_failure_send(MAVLINK_COMM_1 , packet1.app_name , packet1.failure_flags , packet1.expected_version , packet1.actual_version );
-    mavlink_msg_onboard_computer_app_failure_decode(last_msg, &packet2);
+    mavlink_msg_onboard_computer_app_health_send(MAVLINK_COMM_1 , packet1.app_name , packet1.failure_flags , packet1.expected_version , packet1.actual_version );
+    mavlink_msg_onboard_computer_app_health_decode(last_msg, &packet2);
         MAVLINK_ASSERT(memcmp(&packet1, &packet2, sizeof(packet1)) == 0);
 
 #ifdef MAVLINK_HAVE_GET_MESSAGE_INFO
-    MAVLINK_ASSERT(mavlink_get_message_info_by_name("ONBOARD_COMPUTER_APP_FAILURE") != NULL);
-    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_ONBOARD_COMPUTER_APP_FAILURE) != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_name("ONBOARD_COMPUTER_APP_HEALTH") != NULL);
+    MAVLINK_ASSERT(mavlink_get_message_info_by_id(MAVLINK_MSG_ID_ONBOARD_COMPUTER_APP_HEALTH) != NULL);
 #endif
 }
 
@@ -1940,7 +1940,7 @@ static void mavlink_test_auterion(uint8_t system_id, uint8_t component_id, mavli
     mavlink_test_ship_approach_sectors_status(system_id, component_id, last_msg);
     mavlink_test_payload_device_status(system_id, component_id, last_msg);
     mavlink_test_onboard_computer_apps_status(system_id, component_id, last_msg);
-    mavlink_test_onboard_computer_app_failure(system_id, component_id, last_msg);
+    mavlink_test_onboard_computer_app_health(system_id, component_id, last_msg);
 }
 
 #ifdef __cplusplus
